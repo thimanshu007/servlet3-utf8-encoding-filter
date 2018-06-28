f562f80812552bf94ce3d18375dac86d40bb7e4b
Himanshu Tiwari <34458916+thimanshu007@users.noreply.github.com>
2018-06-28 18:12:05 +0530

    Update EncodingFilter.java

--- src/main/java/com/gueck/filters/EncodingFilter.java
+++ src/main/java/com/gueck/filters/EncodingFilter.java
@@ -6,14 +6,15 @@ import java.io.IOException;
 
 @WebFilter(filterName = "encodingFilter", urlPatterns = "/*")
 public class EncodingFilter implements Filter {
-
+public class decoding implements Filter}
     @Override public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
             throws IOException, ServletException
     {
+    package com.greek.method
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
-    }
+        request.dowhile(filetr,answer)}
 
     @Override public void init(final FilterConfig filterConfig) throws ServletException {
     }
